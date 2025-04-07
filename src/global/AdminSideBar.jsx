b2181097fee641cd6b44fdfe0d12655523cc7a8d
lopezmichaelkobe <137591124+lopezmichaelkobe@users.noreply.github.com>
2025-04-07 09:28:23 +0800

    updated adminsidebar

--- src/global/AdminSideBar.jsx
+++ src/global/AdminSideBar.jsx
@@ -54,80 +54,32 @@ const AdminSidebar = () => {
 
           {/* Admin Only: Manage Surveys */}
           {isAdmin && (
-            <li className="nav-item">
-              <a className="nav-link" data-bs-toggle="collapse" href="#surveyMenu" role="button" title="Manage Survey">
-                <i className="bi bi-card-heading"></i>
-                <span className="link-text"> Manage Surveys</span>
-              </a>
-              <div className="collapse" id="surveyMenu">
-                <ul className="nav flex-column ms-3">
                   <li>
-                    <a href="/add-survey" className="nav-link" title="Add Survey">
-                      <i className="bi bi-file-diff"></i>
-                      <span className="link-text"> Add Survey</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/managesurvey" className="nav-link" title="Manage Survey">
-                      <i className="bi bi-table"></i>
-                      <span className="link-text"> Survey</span>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </li>
+                  <a href="/managesurvey" className="nav-link" title="Manage Survey">
+                    <i className="bi bi-card-heading"></i>
+                    <span className="link-text"> Surveys</span>
+                  </a>
+                </li>
           )}
 
           {/* Admin Only: Manage Offices */}
           {isAdmin && (
-            <li className="nav-item">
-              <a className="nav-link" data-bs-toggle="collapse" href="#officeMenu" role="button" title="Manage Office">
-                <i className="bi bi-building-fill"></i>
-                <span className="link-text"> Manage Offices</span>
-              </a>
-              <div className="collapse" id="officeMenu">
-                <ul className="nav flex-column ms-3">
                   <li>
-                    <a href="/add-office" className="nav-link" title="Add Office">
-                      <i className="bi bi-building-add"></i>
-                      <span className="link-text"> Add Office</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/manageoffice" className="nav-link" title="Manage Office">
-                      <i className="bi bi-table"></i>
-                      <span className="link-text"> Office</span>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </li>
+                  <a href="/manageoffice" className="nav-link" title="Manage Office">
+                    <i className="bi bi-building-fill"></i>
+                    <span className="link-text"> Offices</span>
+                  </a>
+                </li>
           )}
 
           {/* Admin Only: Manage Users */}
           {isAdmin && (
-            <li className="nav-item">
-              <a className="nav-link" data-bs-toggle="collapse" href="#usersMenu" role="button" title="Users">
-                <i className="bi bi-people"></i>
-                <span className="link-text"> Manage Users</span>
-              </a>
-              <div className="collapse" id="usersMenu">
-                <ul className="nav flex-column ms-3">
                   <li>
-                    <a href="/add-user" className="nav-link" title="Add User">
-                      <i className="bi bi-person-plus"></i>
-                      <span className="link-text"> Add User</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/manageuser" className="nav-link" title="Manage Users">
-                      <i className="bi bi-gear"></i>
-                      <span className="link-text"> Users</span>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </li>
+                  <a href="/manageuser" className="nav-link" title="Manage Users">
+                    <i className="bi bi-people"></i>
+                    <span className="link-text"> Users</span>
+                  </a>
+                </li>
           )}
           {/* ✅ Logout Button */}
           <li className="nav-item mt-auto">
